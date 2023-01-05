b3bf74eb80e911b6dec10a916bb13247ad7b8fd9
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-05 16:49:17 +0000

    simple list of animals

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+StephanieCobble | Lab 34 Optional Challenge 
+Create a list of at least five animals with three letter names and have your program output them to the screen.
+'''
+
+def main(): 
+    animals_list = ["cat", "dog", "cow", "fox", "hen"]
+    print(f'List of animals with 3 letter names: {animals_list}')
+
+main()
