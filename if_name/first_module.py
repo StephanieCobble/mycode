26647286c4be7bfcefa81b65222189eac64012c0
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-06 18:55:25 +0000

    working with importing files. if name == 'main'

---
+++ if_name/first_module.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+StephanieCobble | Lab 56 - if name == “main”
+'''
+
+print("This code will always execute")
+
+def main(): 
+
+    # print("Module #1 Name=", __name__)
+    print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+    main()
+    # print("Code is being run directly from Python.")
+
+# else:
+    # print("Code is being run indirectly from import.")
---
+++ if_name/second_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+StephanieCobble | Lab 56 - if name == “main” cont
+'''
+
+
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+first_module.main()
+
+print("Module #2 Name=", __name__)
