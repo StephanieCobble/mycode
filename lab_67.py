ff52c4a64366e6500ad1d3fd02bc4c96a562c32c
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-07 00:23:49 +0000

    dracula looping challenege - lab 67

---
+++ lab_67.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+StephanieCobble | Lab 67 - Challenge - Looping Vampires - Spooky Looping’ Vampires
+1. Read in the content of the Dracula novel as a file object.
+2. Loop over every line in Dracula, print each line out
+3. Actually, fix that for loop... only print out the line if it has the word vampire in it
+4. If you didn't already, make sure it prints the line no matter what case vampire is
+5. Count that up! How many lines contain the word vampire?
+6. Take the lines from Dracula that have vampire in it and write them to a second file named vampytimes.txt
+'''
+def main():
+
+    counter = 0
+    with open("dracula.txt","r") as book:
+        with open("vampytimex.txt","w") as vamp:
+            for line in book:
+                if "vampire" in line.lower():
+                    print(line)
+                    counter += 1
+                    vamp.write(line)
+    print(counter)
+
+if __name__ == "__main__":
+    main()
