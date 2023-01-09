645c282795f1736d4c6cff1b63c26f421627f26c
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-09 18:23:50 +0000

    best practice and pylint

---
+++ smallscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+StephanieCobble | Lab 71 - Best Practice & pylint
+'''
+def main():
+    '''main program'''
+    mynote = 'print this string'
+    print(mynote)
+
+if __name__ == "__main__":
+    main()
