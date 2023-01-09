379f47d7ed8f3330a891c69eb1eb1900a02fd7d7
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-09 18:05:51 +0000

    Lab 68 - scripting commands with python; Lab 69 - Creating functions; & Lab 70 - defining functions & using crayons

---
+++ color/color2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+'''
+Alta3 Research || Author RZFeeser@alta3.com
+StephanieCobble | Lab 70 - Defining Function
+Learning how to use functions
+'''
+
+## Installs the crayons package.
+## python3 -m pip install crayons
+## import statements ALWAYS go up top
+import crayons
+
+
+def main():
+    '''run time code. Always indent under function'''
+
+    # print 'red string' in red
+    print(crayons.red('red string'))
+
+    # Red White and Blue text
+    #print('{} white {}'.format(crayons.red('red'), crayons.blue('blue'))) # format string (old ver of str templating)
+    print(f"{crayons.red('red')} white {crayons.blue('blue')}")  # f-string (newest version of str templating)
+
+    crayons.disable() # disables the crayons package
+
+    # this line should NOT have color as crayons is disabled
+    print(f"{crayons.red('red')} white {crayons.blue('blue')}")  # f-string (newest version of string templating)
+
+    crayons.DISABLE_COLOR = False # enable the crayons package
+
+    # This line will print in color because color is enabled
+    print(f"{crayons.red('red')} white {crayons.blue('blue')}")  # f-string (newest version of string templating)
+
+    # print 'red string' in red
+    print(crayons.red('red string', bold=True))
+
+    # print 'yellow string' in yellow
+    print(crayons.yellow('yellow string', bold=True))
+
+    # print 'magenta string' in magenta
+    print(crayons.magenta('magenta string', bold=True))
+
+    # print 'white string' in white
+    print(crayons.white('white string', bold=True))
+
+    # Write a line that prints your name in at least two colors
+    print(f'My name is {crayons.magenta("Stephanie")} {crayons.green("Cobble")}')
+
+# this condition is only true if our script is run directly
+# it is NOT true if our code is imported into another script
+if __name__ == "__main__":
+    main()
+
---
+++ netfunct01/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''
+Alta3 Research || Author: RZFeeser@alta3.com
+StephanieCobble | Lab 69 - Creating functions
+'''
+
+import crayons
+import json
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==dict
+
+    for ip in devicecmd.keys(): # looping through the dict
+        print(f'{crayons.magenta("Handshaking")}. .. ... connecting with {ip}') # fstring
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'Attempting to sending command --> {mycmds}')
+            # we'll learn to write code that sends cmds to device here
+    return None
+
+# start our main script
+def main():
+    '''called at runtime'''
+
+    # dict containing IPs mapped to a list of physical interfaces and their state
+    devicecmd = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]}
+
+
+    # using JSON
+    # with open("~/netfunct01/devicecmd.json", "r") as devicecmdfile:
+        # devicecmd = json.load(devicecmdfile)
+
+    print(f"Welcome to the {crayons.cyan('network')} device command pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(devicecmd) # call function to push commands to devices
+
+# call our main function
+main()
+
