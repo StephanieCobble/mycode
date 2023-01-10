9b975394dcca4a4fb84e8c20b5e8e12217671bd0
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-10 18:57:16 +0000

    more pandas work

--- pandas/pandabear01.py
+++ pandas/pandabear01.py
@@ -37,6 +37,32 @@ def main():
     # export 5 movies from the top of the dataframe to csv
     movies_sheet1.head(5).to_csv("5movies.csv")
 
+    
+    ## Lab 81 - Pandas Dataframes w/ Excel, csv, json, HTML, & beyond
+    # grab the next 2 sheets as well
+    movies_sheet2 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
+    # DF has 5 rows and 24 columns (indexed by title)
+    print(movies_sheet2.head())
+
+    movies_sheet3 = pd.read_excel(excel_file, sheet_name=2, index_col=0)
+    # DF has 5 rows and 24 columns (indexed by title)
+    print(movies_sheet3.head())
+
+    # combine all DFs into a single DF called movies
+    movies = pd.concat([movies_sheet1, movies_sheet2, movies_sheet3])
+
+    # number of rows and columns (5042, 24)
+    print(movies.shape)
+
+    # sort DataFrame based on Gross Earnings
+    sorted_by_gross = movies.sort_values(["Gross Earnings"], ascending=False)
+
+    # Data is sorted by values in a column
+    # display the top 10 movies by Gross Earnings.
+    # passing the 10 values to head returns the top 10 not the default 5
+    print(sorted_by_gross.head(10))
+
+
 
 if __name__ == "__main__":
     main()
