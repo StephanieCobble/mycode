624d063593d11d38154036c047d54d6dbf5e0acb
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-10 22:43:46 +0000

    reiew on how to manipulate JSON data with python

---
+++ makejson02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+The json.dumps() function creates a JSON string | Alta3 Research
+StephanieCobble | Lab 87 - Python data to JSON
+"""
+
+# JSON is part of the Python Standard Library
+import json
+
+def main():
+    """runtime code"""
+    ## create a blob of data to work with
+    hitchhikers = [{"name": "Zaphod Beeblebrox", "species": "Betelgeusian"},
+      {"name": "Arthur Dent", "species": "Human"}]
+
+    ## display our Python data (a list containing two dictionaries)
+    print(hitchhikers)
+
+    ## Create the JSON string
+    jsonstring = json.dumps(hitchhikers)
+
+    ## Display a single string of JSON
+    print(jsonstring)
+
+if __name__ == "__main__":
+    main()
+
---
+++ makejson04.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+opening a static file containing JSON data | Alta3 Research
+StephanieCobble | Lab 87 - Python data to JSON
+using .load() amd .loads()
+"""
+
+# JSON is part of the Python Standard Library
+import json
+
+def main():
+    """runtime code"""
+    ## open the file
+    with open("datacenter.json", "r") as datacenter:
+        datacenterdecoded = json.load(datacenter)
+
+    ## This is now a dictionary
+    print(type(datacenterdecoded))
+
+    ## display the servers in the datacenter
+    print(datacenterdecoded)
+
+    ## display the servers in row3
+    print(datacenterdecoded["row3"])
+
+    ## display the 2nd server in row2
+    print(datacenterdecoded["row2"][1])
+
+if __name__ == "__main__":
+    main()
+
