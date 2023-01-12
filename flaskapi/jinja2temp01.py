bb6907756c4830d62c5ac972934eff547156f04b
StephanieCobble <cobble.stephanie@gmail.com>
2023-01-12 22:12:25 +0000

    Lab 105 - Jinja templating for HTML and configs

---
+++ flaskapi/jinja2temp01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+StephanieCobble - Lab 105 - Flask APIs & Jinja
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("hellobasic.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
+
